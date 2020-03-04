d846524deb60c26a46a7244aa25b68c21f481dd5
Sergey Ponkrashin <sergey.ponkrashin@arcadia.spb.ru>
2020-03-04 18:23:50 +0300

    Initial commit - empty ASP.NET Core project.

---
+++ ArcadiaTeamsBot/ArcadiaTeamsBot/Startup.cs
@@ -0,0 +1,33 @@
+namespace ArcadiaTeamsBot
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
