c696620141353f3ff4e2090916fa6aac6442b74a
laska_sl <lasukovv902@gmail.com>
2020-03-30 15:09:56 +0300

    Added DTOs

---
+++ ServiceDesk.Abstractions/DTOs/RequestTypeDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceDesk.Abstractions.DTOs
+{
+    public class RequestTypeDTO
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string Description { get; set; }
+
+        public RequestTypeFieldDTO[] RequestTypeFields { get; set; }
+    }
+}
---
+++ ServiceDesk.Abstractions/DTOs/RequestTypeFieldDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceDesk.Abstractions.DTOs
+{
+    public class RequestTypeFieldDTO
+    {
+        public int Id { get; set; }
+
+        public string FieldName { get; set; }
+
+        public bool IsMandatory { get; set; }
+    }
+}
