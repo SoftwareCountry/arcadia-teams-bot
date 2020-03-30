ad68e9e00e062998ada39eae314078f6390a51f7
laska_sl <lasukovv902@gmail.com>
2020-03-30 16:29:31 +0300

    Implemented getting priorities

---
+++ ArcadiaTeamsBot.CQRS/Handlers/GetServiceDeskRequestPrioritiesHandler.cs
@@ -0,0 +1,36 @@
+﻿using ArcadiaTeamsBot.CQRS.Queries;
+using MediatR;
+using Newtonsoft.Json;
+using ServiceDesk.Abstractions.DTOs;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArcadiaTeamsBot.CQRS.Handlers
+{
+    class GetServiceDeskRequestPrioritiesHandler : IRequestHandler<GetServiceDeskRequestPrioritiesQuery, IEnumerable<ServiceDeskRequestPriorityDTO>>
+    {
+        public async Task<IEnumerable<ServiceDeskRequestPriorityDTO>> Handle(GetServiceDeskRequestPrioritiesQuery request, CancellationToken cancellationToken)
+        {
+            var prioritiesJsonAsString = 
+                @"[
+                        {
+                        'key': 1,
+                        'value': 'Low'
+                        },
+                        {
+                        'key': 2,
+                        'value': 'Default'
+                        },
+                        {
+                        'key': 3,
+                        'value': 'High'
+                        }
+                ]";
+
+            var serviceDeskRequestPriorities = JsonConvert.DeserializeObject<IEnumerable<ServiceDeskRequestPriorityDTO>>(prioritiesJsonAsString);
+
+            return serviceDeskRequestPriorities;
+        }
+    }
+}
