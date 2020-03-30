a536dbfa6e08826c2c64a2364a1e5626643c8920
laska_sl <lasukovv902@gmail.com>
2020-03-30 16:29:47 +0300

    Implemented getting request types

---
+++ ArcadiaTeamsBot.CQRS/Handlers/GetServiceDeskRequestTypesHandler.cs
@@ -0,0 +1,40 @@
+﻿using ArcadiaTeamsBot.CQRS.Queries;
+using MediatR;
+using ServiceDesk.Abstractions.DTOs;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ArcadiaTeamsBot.CQRS.Handlers
+{
+    class GetServiceDeskRequestTypesHandler : IRequestHandler<GetServiceDeskRequestTypesQuery, IEnumerable<ServiceDeskRequestTypeDTO>>
+    {
+        private const string requestTypesUrl = "https://arcadia-servicedesk-dev31.azurewebsites.net/api/intra/requestTypes";
+
+        public GetServiceDeskRequestTypesHandler(IHttpClientFactory clientFactory)
+        {
+            ClientFactory = clientFactory;
+        }
+
+        public IHttpClientFactory ClientFactory { get; }
+
+        public async Task<IEnumerable<ServiceDeskRequestTypeDTO>> Handle(GetServiceDeskRequestTypesQuery request, CancellationToken cancellationToken)
+        {
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, requestTypesUrl);
+
+            httpRequest.Headers.Add("x-api-key", "not-installed");
+
+            var client = ClientFactory.CreateClient();
+
+            var response = await client.SendAsync(httpRequest, cancellationToken);
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            var serviceDeskRequestTypes = JsonConvert.DeserializeObject<IEnumerable<ServiceDeskRequestTypeDTO>>(responseBody);
+
+            return serviceDeskRequestTypes;
+        }
+    }
+}
