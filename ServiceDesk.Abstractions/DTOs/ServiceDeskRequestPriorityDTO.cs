8b5a4032676f2068feb3cf6bcc0388d08249e2fb
laska_sl <lasukovv902@gmail.com>
2020-04-02 08:22:21 +0300

    Removed old projects after renaming

--- ServiceDesk.Abstractions/DTOs/ServiceDeskRequestPriorityDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ArcadiaTeamsBot.ServiceDesk.Abstractions.DTOs
-{
-    public class ServiceDeskRequestPriorityDTO
-    {
-        public int Key { get; set; }
-
-        public string Value { get; set; }
-    }
-}
