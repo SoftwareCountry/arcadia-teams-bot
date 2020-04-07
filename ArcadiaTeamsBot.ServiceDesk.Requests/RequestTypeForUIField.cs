70efa28e2db25bdad764ee77368c0015b50db661
laska_sl <lasukovv902@gmail.com>
2020-04-07 12:11:09 +0300

    Added request type class

---
+++ ArcadiaTeamsBot.ServiceDesk.Requests/RequestTypeForUIField.cs
@@ -0,0 +1,14 @@
+﻿namespace ArcadiaTeamsBot.ServiceDesk.Requests
+{
+    public class RequestTypeForUIField
+    {
+        public RequestTypeForUIField(string fieldName, RequestTypeForUIFieldType fieldType)
+        {
+            this.FieldName = fieldName;
+            this.FieldType = fieldType;
+        }
+        public string FieldName { get; set; }
+
+        public RequestTypeForUIFieldType FieldType { get; set; }
+    }
+}
