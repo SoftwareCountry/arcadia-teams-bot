d0cb507ac5f9c8ae586997c30f0eac7831d20c71
laska_sl <lasukovv902@gmail.com>
2020-04-11 08:47:28 +0300

    Added command&handler for new request creation

---
+++ ArcadiaTeamsBot.CQRS.Abstractions/Commands/CreateNewServiceDeskRequestCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace ArcadiaTeamsBot.CQRS.Abstractions.Commands
+{
+    using ArcadiaTeamsBot.ServiceDesk.Abstractions.DTOs;
+
+    using MediatR;
+
+    public class CreateNewServiceDeskRequestCommand : IRequest
+    {
+        public CreateNewServiceDeskRequestCommand(RequestForCreationDTO requestForCreationDTO)
+        {
+            this.RequestForCreationDto = requestForCreationDTO;
+        }
+
+        public RequestForCreationDTO RequestForCreationDto { get; }
+    }
+}
---
+++ ArcadiaTeamsBot.CQRS/Handlers/CreateNewServiceDeskRequestHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace ArcadiaTeamsBot.CQRS.Handlers
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using ArcadiaTeamsBot.CQRS.Abstractions.Commands;
+    using ArcadiaTeamsBot.ServiceDesk.Abstractions;
+
+    using MediatR;
+
+    internal class CreateNewServiceDeskRequestHandler : IRequestHandler<CreateNewServiceDeskRequestCommand>
+    {
+        private readonly IServiceDeskClient serviceDeskClient;
+
+        public CreateNewServiceDeskRequestHandler(IServiceDeskClient serviceDeskClient)
+        {
+            this.serviceDeskClient = serviceDeskClient;
+        }
+
+        public async Task<Unit> Handle(CreateNewServiceDeskRequestCommand request, CancellationToken cancellationToken)
+        {
+            await this.serviceDeskClient.CreateNewRequest(request.RequestForCreationDto, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
