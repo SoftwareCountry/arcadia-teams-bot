06c789b02126d0bd8f14bcccc8aa64e4fad92ded
laska_sl <lasukovv902@gmail.com>
2020-04-25 13:15:14 +0300

    Fixed DTOs for creation

--- ArcadiaTeamsBot.ServiceDesk.Abstractions/DTOs/CreateRequestTypeDTO.cs
+++ ArcadiaTeamsBot.ServiceDesk.Abstractions/DTOs/CreateRequestTypeDTO.cs
@@ -1,7 +1,12 @@
 ﻿namespace ArcadiaTeamsBot.ServiceDesk.Abstractions.DTOs
 {
+    using System.Collections.Generic;
+
     public class CreateRequestTypeDTO
     {
         public int Id { get; set; }
+        
+        public IEnumerable<CreateRequestTypeFieldDTO> RequestTypeFields { get; set; }
+
     }
 }
---
+++ ArcadiaTeamsBot.ServiceDesk.Abstractions/DTOs/CreateRequestTypeFieldDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ArcadiaTeamsBot.ServiceDesk.Abstractions.DTOs
+{
+    public class CreateRequestTypeFieldDTO
+    {
+        public int Id { get; set; }
+
+        public string FieldName { get; set; }
+
+        public bool IsMandatory { get; set; }
+    }
+}
